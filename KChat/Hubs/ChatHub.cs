0b3eb36072620bf1dac3a5a6978d4bbdf689a880
pe-pu <nagasawa.katsuya2022@gmail.com>
2022-08-10 16:20:55 +0900

    夏休みだよ全員集合

--- KChat/Hubs/ChatHub.cs
+++ KChat/Hubs/ChatHub.cs
@@ -8,17 +8,17 @@ namespace KChat.Hubs
 {
     public class ChatHub:Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessage(string user, string message)　//全体に向けてメッセージ送信（管理者からのメッセージとかで使うとおもしろいかも）
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
 
-        public async Task SendMessageToGroup(string group, string user, string message)
+        public async Task SendMessageToGroup(string group, string user, string message)//グループに向けてメッセージ送信
         {
             await Clients.Group(group).SendAsync("ReceiveMessage", user, message);
         }
 
-        public async Task AddToGroup(string groupName)
+        public async Task AddToGroup(string groupName)  //グループに追加する処理
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
         }
